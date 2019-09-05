b867775dd5bc83e295f9d63f30f265415f1b11e2
Nukk Pointer <nukkpointer@gmail.com>
2019-09-05 21:11:27 +0100

    Logic to call other service for lights

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from scapy.all import *
 from scapy.layers.l2 import ARP
+from requests import *
+
+#Initial state of light
+lightOn = True
+
+def callHueController():
+    global lightOn
+    print(lightOn)
+    if lightOn:
+        print("turning light off")
+        get('http://192.168.0.80:1138/1251/bedroomLight/off')
+        lightOn = False
+    else:
+        print("turning light on")
+        get('http://192.168.0.80:1138/1251/bedroomLight/on')
+        lightOn = True
 
 
 def arp_monitor_callback(pkt):
     if ARP in pkt and pkt[ARP].op in (1, 2):  # who-has or is-at
         print("sniffed")
         #call 192.168.0.80
+        callHueController()
         return pkt.sprintf("%ARP.hwsrc% %ARP.psrc%")
 
 
